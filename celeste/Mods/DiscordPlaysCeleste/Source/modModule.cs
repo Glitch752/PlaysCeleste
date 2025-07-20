b57332e58da57edad29772e4c3c155e4540f0d5a
Glitch752 <xxGlitch752xx@gmail.com>
2025-07-19 23:54:51 -0500

    Move mod because it makes Everest happier

--- celeste/Mods/DiscordPlaysCeleste/Source/modModule.cs
+++ celeste/Mods/DiscordPlaysCeleste/Source/modModule.cs
@@ -1,21 +1,24 @@
 ﻿using System;
+using Monocle;
 
 namespace Celeste.Mod.mod;
 
-public class modModule : EverestModule {
+public class modModule : EverestModule
+{
     public static modModule Instance { get; private set; }
 
     // See https://github.com/EverestAPI/Resources/wiki/Code-Mod-Setup#mod-settings-session-and-save-data
     public override Type SettingsType => typeof(modModuleSettings);
-    public static modModuleSettings Settings => (modModuleSettings) Instance._Settings;
+    public static modModuleSettings Settings => (modModuleSettings)Instance._Settings;
 
     public override Type SessionType => typeof(modModuleSession);
-    public static modModuleSession Session => (modModuleSession) Instance._Session;
+    public static modModuleSession Session => (modModuleSession)Instance._Session;
 
     public override Type SaveDataType => typeof(modModuleSaveData);
-    public static modModuleSaveData SaveData => (modModuleSaveData) Instance._SaveData;
+    public static modModuleSaveData SaveData => (modModuleSaveData)Instance._SaveData;
 
-    public modModule() {
+    public modModule()
+    {
         Instance = this;
 #if DEBUG
         // debug builds use verbose logging
@@ -28,18 +31,32 @@ public class modModule : EverestModule {
 
     // Set up any hooks, event handlers and your mod in general here.
     // Load runs before Celeste itself has initialized properly.
-    public override void Load() {
+    public override void Load()
+    {
+        On.Monocle.Engine.RenderCore += RenderCore;
+    }
+
+    protected virtual void RenderCore(On.Monocle.Engine.orig_RenderCore orig, Engine self)
+    {
+        // Log to the console
+        Logger.Log(nameof(modModule), "RenderCore called");
+
+        orig(self);
     }
 
     // Optional, initialize anything after Celeste has initialized itself properly.
-    public override void Initialize() {
+    public override void Initialize()
+    {
     }
 
     // Optional, do anything requiring either the Celeste or mod content here.
-    public override void LoadContent(bool firstLoad) {
+    public override void LoadContent(bool firstLoad)
+    {
     }
 
     // Unload the entirety of your mod's content. Free up any native resources.
-    public override void Unload() {
+    public override void Unload()
+    {
+        On.Monocle.Engine.RenderCore -= RenderCore;
     }
 }
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# Depends on Celeste being in the celeste/ folder
+
 curl https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/4923/artifacts?artifactName=linux.main&api-version=5.0&%24format=zip --output olympus.zip
 unzip olympus.zip
 rm olympus.zip
@@ -9,8 +11,11 @@ rm dist.zip
 cd ..
 mv linux.main/ olympus/
 
-echo "\n\nDownloaded olympus. You'll need to go into the UI and set up the Celeste location and install Everest (testing done on version 5635)."
+echo "\n\nDownloaded olympus. You'll need to go into the UI and set up the Celeste location and install Everest (testing done on version 5635).\nClose the application when ready."
 
 olympus/olympus
 
 # TODO: Manual Everest installation without Olympus GUI
+
+# Replace "CodeReload_WIP: false" in $XDG_DATA_HOME/Celeste/Saves/modsettings-Everest.celeste (or ~/.local/share/...) with true
+sed -i 's/CodeReload_WIP: false/CodeReload_WIP: true/' "${XDG_DATA_HOME:-$HOME/.local/share}/Celeste/Saves/modsettings-Everest.celeste"
